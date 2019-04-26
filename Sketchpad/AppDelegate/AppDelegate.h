adc7ffbba144888f340b6a1910d757b674e77d8d
Alvin <ling94alvin@gmail.com>
2019-04-26 16:34:56 -0500

    add sketchpad proj

---
+++ Sketchpad/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sketchpad
+//
+//  Created by Alvin Ling on 4/26/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sketchpad/AppDelegate/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  Sketchpad
+//
+//  Created by Alvin Ling on 4/26/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
