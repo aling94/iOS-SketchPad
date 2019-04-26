bd41de6a8a7e4e51dbbbf0e562ce1f3c673eac74
Alvin <ling94alvin@gmail.com>
2019-04-26 16:52:33 -0500

    custom view for color picking

---
+++ Sketchpad/View/ColorPickerView.h
@@ -0,0 +1,17 @@
+//
+//  ColorPickerView.h
+//  Sketchpad
+//
+//  Created by Alvin Ling on 4/26/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ColorPickerView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sketchpad/View/ColorPickerView.m
@@ -0,0 +1,21 @@
+//
+//  ColorPickerView.m
+//  Sketchpad
+//
+//  Created by Alvin Ling on 4/26/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import "ColorPickerView.h"
+
+@implementation ColorPickerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
