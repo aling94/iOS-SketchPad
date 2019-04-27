0f793e7584c26aa9b8c8b8d72381f073ff909add
Alvin <ling94alvin@gmail.com>
2019-04-27 13:31:27 -0500

    add settings vc

---
+++ Sketchpad/Controller/SettingsViewController.h
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.h
+//  Sketchpad
+//
+//  Created by Alvin Ling on 4/27/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sketchpad/Controller/SettingsViewController.m
@@ -0,0 +1,33 @@
+//
+//  SettingsViewController.m
+//  Sketchpad
+//
+//  Created by Alvin Ling on 4/27/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *preview;
+
+@end
+
+@implementation SettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
