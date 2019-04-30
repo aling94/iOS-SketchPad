8921481774cb47d59d51e96e99d5775e6a3a2133
Alvin <ling94alvin@gmail.com>
2019-04-30 10:50:01 -0500

    add page for showing user feed of drawings

---
+++ Sketchpad/Controller/FeedViewController.h
@@ -0,0 +1,17 @@
+//
+//  FeedViewController.h
+//  Sketchpad
+//
+//  Created by Alvin Ling on 4/30/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FeedViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
